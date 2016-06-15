74b146309d0ea9f2f56b5554c4afeafd398b5581
zhenqing.wang <wzqwsrf@126.com>
2016-06-15 15:30:47 +0800

    add 生产者消费者

---
+++ src/main/java/com/wzq/core/thread/Producer.java
@@ -0,0 +1,43 @@
+package com.wzq.core.thread;
+
+import java.util.Date;
+import java.util.Vector;
+
+/**
+ * Author: zhenqing.wang <wangzhenqing1008@163.com>
+ * Date: 2016-06-15 15:13:34
+ * Description: 生产者
+ */
+public class Producer extends Thread {
+    private Vector messages = new Vector();
+    private static final int MAXQUEUE = 5;
+
+    public void run() {
+        while (true) {
+            try {
+                putMessage();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public synchronized void putMessage() throws InterruptedException {
+        if (messages.size() == MAXQUEUE) {
+            wait();
+        }
+        messages.addElement(new Date().toString());
+        System.out.println("put message");
+        notify();
+    }
+
+    public synchronized String getMessage() throws InterruptedException {
+        notify();
+        if (messages.size() == 0) {
+            wait();
+        }
+        String message = (String) messages.firstElement();
+        messages.remove(message);
+        return message;
+    }
+}
