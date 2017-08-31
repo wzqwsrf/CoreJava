5f11c6eb77d282a1b061caa8acf4a776a859f266
wzqwsrf <wzqwsrf@126.com>
2017-08-31 17:29:48 +0800

    add heap sort

---
+++ src/main/java/com/wzq/core/sort/HeapSort.java
@@ -0,0 +1,71 @@
+package com.wzq.core.sort;
+
+import java.util.Random;
+
+/**
+ * Author: zhenqing.wang <wangzhenqing1008@163.com>
+ * Date: 2017-8-31 17:27:52
+ * Description: 堆排序实现，本例为最大堆实现
+ */
+public class HeapSort {
+    private int array[];
+
+    public HeapSort(int len) {
+        array = new int[len];
+        Random random = new Random();
+        for (int i = 0; i < len; i++) {
+            array[i] = random.nextInt(20);
+        }
+    }
+
+    public void buildMaxHeap() {
+        int len = array.length;
+        int index = len / 2 - 1;
+        for (int i = index; i >= 0; i--) {
+            maxHeapify(i, len);
+        }
+        printArray();
+    }
+
+    private void maxHeapify(int index, int len) {
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+        int max = index;
+        if (left < len && array[max] < array[left]) {
+            max = left;
+        }
+        if (right < len && array[max] < array[right]) {
+            max = right;
+        }
+        if (max != index) {
+            int temp = array[index];
+            array[index] = array[max];
+            array[max] = temp;
+            maxHeapify(max, len);
+        }
+    }
+
+    public void heapSort() {
+        buildMaxHeap();
+        int len = array.length;
+        for (int i = len - 1; i > 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            maxHeapify(0, i);
+        }
+    }
+
+    public void printArray() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        HeapSort heap = new HeapSort(10);
+        heap.heapSort();
+        heap.printArray();
+    }
+}
