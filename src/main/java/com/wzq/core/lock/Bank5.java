88bc5c42b3013f6312c2d88f854365c2b58dfca2
wzqwsrf <wzqwsrf@126.com>
2017-10-26 17:45:59 +0800

    fix synchronized to lock

---
+++ src/main/java/com/wzq/core/lock/Bank5.java
@@ -0,0 +1,34 @@
+package com.wzq.core.lock;
+
+/**
+ * Author: zhenqing.wang <wangzhenqing1008@163.com>
+ * Date: 2016-06-14 18:26:33
+ * Description: ThreadLocal银行
+ */
+public class Bank5 {
+    private static ThreadLocal<Integer> threadLocal = new ThreadLocal<Integer>() {
+        @Override
+        protected Integer initialValue() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+    };
+
+    public void addMoney(int money) {
+        threadLocal.set(threadLocal.get() + money);
+        System.out.println(System.currentTimeMillis() + "存进" + money);
+    }
+
+    public void subMoney(int money) {
+        if (threadLocal.get() < money) {
+            System.out.println("余额不足");
+            return;
+        }
+        threadLocal.set(threadLocal.get() - money);
+        System.out.println(System.currentTimeMillis() + "取出" + money);
+    }
+
+    public void getCurrentMoney() {
+        System.out.println("当前余额为" + threadLocal.get());
+    }
+}
---
+++ src/main/java/com/wzq/core/lock/Main.java
@@ -0,0 +1,46 @@
+package com.wzq.core.lock;
+
+/**
+ * Author: zhenqing.wang <wangzhenqing1008@163.com>
+ * Date: 2016-06-14 18:29:56
+ * Description: Main函数
+ */
+public class Main {
+    public static void main(String[] args) {
+        final Bank bank = new Bank();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    bank.addMoney(100);
+                    bank.getCurrentMoney();
+                    System.out.println("=========");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    bank.subMoney(200);
+                    bank.getCurrentMoney();
+                    System.out.println("×××××××××");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
+}
